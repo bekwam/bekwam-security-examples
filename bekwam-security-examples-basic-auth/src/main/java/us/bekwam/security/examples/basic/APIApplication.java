00d21f2d5ae00cf5072d68a36d64c309fdc84d2c
CarlW <carl@bekwam.com>
2020-11-09 08:58:26 -0500

    Initial import

---
+++ bekwam-security-examples-basic-auth/src/main/java/us/bekwam/security/examples/basic/APIApplication.java
@@ -0,0 +1,8 @@
+package us.bekwam.security.examples.basic;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class APIApplication extends Application {
+}
