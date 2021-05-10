6f6d725982187f388a90301fb7f979a3d535fc24
Carl Walker <carl@bekwam.com>
2021-05-10 13:25:38 -0400

    Updated example for DIGEST-MD5

---
+++ bekwam-security-examples-basic-auth/src/test/java/us/bekwam/security/examples/basic/EchoBeanTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2021 Bekwam, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package us.bekwam.security.examples.basic;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import java.util.Hashtable;
+
+/**
+ * @author carl
+ */
+public class EchoBeanTest {
+
+    public static void main(String[] args) throws Exception {
+        final Hashtable<String, String> jndiProperties = new Hashtable<>();
+        jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY,
+                "org.wildfly.naming.client.WildFlyInitialContextFactory");
+        final Context context = new InitialContext(jndiProperties);
+        Echo echoService = (Echo) context.lookup("ejb:/bekwam-security-examples-basic-auth/EchoBean!us.bekwam.security.examples.basic.Echo");
+        System.out.println( echoService.echo("Hi There!") );
+    }
+}
